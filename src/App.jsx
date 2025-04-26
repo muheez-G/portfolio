64d168dd6c5fc5cbd005019a9fe02ecf8743306c
MDevHub <musbaudeenabdulrahman040@gmail.com>
2025-04-26 18:26:26 +0100

    Into Part of myself

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import Intro from "./components/Intro/intro"
 import Navbar from "./components/NavBar/navbar"
 
 function App() {
-  
+
   return (
     <div className="App">
       <Navbar />
+      <Intro />
     </div>
   )
 }
