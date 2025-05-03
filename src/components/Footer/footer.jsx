7fa3e64e13babe131fe36bc31918066dd6e36c0e
Muheez <ashogbonmuheez@gmail.com>
2025-05-03 11:37:28 +0100

    updated footer

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import './footer.css'
+import reactIcon from '../../assets/images/react.svg'
 
 const Footer = () => {
    return (
       <footer className='footer'>
+         <p className="footer-p">Built with <b> React <img src={reactIcon} alt="React-Icon" /> </b> with ❤️</p>
          Copyright &#169; 2025 Ashogbon Muheez. All right reserved.
       </footer>
    )
